97390f93ab462ea8398728d87af88c11c2cb1523
TaigaAranami <splat.code.002@gmail.com>
2025-05-15 11:37:10 +0900

    SelectLevelのC++を作り直し

---
+++ Source/AchiLess/SelectLevelActor.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SelectLevelActor.h"
+#include "HAL/FileManager.h"
+#include "Misc/Paths.h"
+#include "ADataManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Blueprint/WidgetBlueprintLibrary.h"
+
+// Sets default values
+ASelectLevelActor::ASelectLevelActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASelectLevelActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	LoadAllJson();
+}
+
+void ASelectLevelActor::LoadAllJson()
+{
+	TArray<FString> FilePaths;
+
+	FString Directory(FPaths::ProjectContentDir() + "Json");
+
+	IFileManager::Get().FindFiles(FilePaths, *Directory, true, false);
+
+	UADataManager* DataManager = NewObject<UADataManager>();
+
+	for (const FString FilePath : FilePaths)
+	{
+		FDataStruct Data;
+		DataManager->ReadJsonData(FilePath, Data);
+		AchiLessParams.Add(Data);
+	}
+}
+
+// Called every frame
+void ASelectLevelActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
