30fde4cb95e6e2530929072635045bd95c69d7d5
TaigaAranami <splat.code.002@gmail.com>
2025-05-20 11:34:02 +0900

    カードの情報をCSVファイルから入力できるように

---
+++ Source/AchiLess/CardData.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "CardData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FCardData : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 CardID;
+
+    //カード名
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FText Name;
+
+    //フレーバーテキスト
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FText FravorText;
+
+    // カード画像
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TSoftObjectPtr<UTexture2D> CardImage;
+
+    // 関数名として呼び出す
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FName CardFunctionName;
+
+    // タグ
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TArray<FName> Tags;
+};
